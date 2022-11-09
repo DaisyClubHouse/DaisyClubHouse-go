dd46613f67db62a15dac7172d9ff0a031da4dfb2
付建邦 <fujianbang0ops@gmail.com>
2022-11-09 23:05:54 +0800

    修改创建房间请求参数

--- infrastructure/server/handler/handler.go
+++ infrastructure/server/handler/handler.go
@@ -32,13 +32,26 @@ func (handler *HttpServerHandler) GetRoomProfileListHandler() func(*gin.Context)
 	}
 }
 
+type CreateRoomRequest struct {
+	UserID   string `json:"user_id"`
+	UserName string `json:"user_name"`
+}
+
 func (handler *HttpServerHandler) CreateRoom() func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
 		// 获取玩家信息
-		id := ctx.Request.Header.Get("X-Game-Gobang-UserID")
-		name := ctx.Request.Header.Get("X-Game-Gobang-UserName")
+		var req CreateRoomRequest
+		if err := ctx.ShouldBindJSON(&req); err != nil {
+			ctx.JSON(200, gin.H{
+				"code": -1,
+				"msg":  "请求参数绑定错误",
+			})
+			return
+		}
+		// 提取玩家ID，name
+		id, name := req.UserID, req.UserName
 
-		fmt.Printf("[HTTP] 创建房间请求（id:%s, name:%s）", id, name)
+		fmt.Printf("[HTTP] 创建房间请求（player_id:%s, player_name:%s）", id, name)
 		roomProfile, err := handler.game.CreateRoom(&entity.UserInfo{
 			ID:         id,
 			Username:   name,
